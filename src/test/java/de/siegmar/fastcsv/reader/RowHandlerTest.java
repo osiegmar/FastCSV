77804a8ec33b5313a4f7d71979d5a014f7936b5c
Oliver Siegmar <oliver@siegmar.org>
2020-09-06 13:54:45 +0200

    Add RowHandlerTest

---
+++ src/test/java/de/siegmar/fastcsv/reader/RowHandlerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Oliver Siegmar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.siegmar.fastcsv.reader;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class RowHandlerTest {
+
+    @Test
+    public void test() {
+        final RowHandler rh = new RowHandler(1);
+        rh.add("foo".toCharArray(), 0, 3, 1);
+        rh.add("bar".toCharArray(), 0, 3, 1);
+
+        assertArrayEquals(new String[]{"foo", "bar"}, rh.end());
+        assertEquals(1, rh.getLines());
+    }
+
+}
