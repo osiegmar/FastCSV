24f6fabf98fa49bc41a23699d53b48bce1c3e3b6
Oliver Siegmar <oliver@siegmar.org>
2020-11-15 10:57:18 +0100

    add skipEmptyLines test to generic test

--- src/test/java/de/siegmar/fastcsv/reader/GenericDataTest.java
+++ src/test/java/de/siegmar/fastcsv/reader/GenericDataTest.java
@@ -18,17 +18,18 @@ package de.siegmar.fastcsv.reader;
 
 import static de.siegmar.fastcsv.reader.CharacterConv.parse;
 import static de.siegmar.fastcsv.reader.CharacterConv.print;
-import static de.siegmar.fastcsv.reader.TestUtil.readAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -36,13 +37,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class GenericDataTest {
 
     private static final Pattern LINE_PATTERN =
-        Pattern.compile("^(?<source>[^ ]+)([ ]+(?<expected>[^ ]+))*");
+        Pattern.compile("^(?<source>\\S+)(?:\\s+(?<expected>\\S+))?(?:\\s+\\[(?<flags>\\w+)])?");
 
     @ParameterizedTest
     @MethodSource("dataProvider")
     public void dataTest(final TestData data) {
         final String expected = print(data.getExpected());
-        final String actual = print(readAll(parse(data.getSource())));
+        final String actual = print(readAll(parse(data.getSource()), data.isSkipEmptyLines()));
         assertEquals(expected, actual, () -> "Error in line: '" + data.getLine() + "'");
     }
 
@@ -59,7 +60,9 @@ public class GenericDataTest {
                 if (matcher.matches()) {
                     final String source = matcher.group("source");
                     final String expected = matcher.group("expected");
-                    data.add(new TestData(line, source, expected != null ? expected : source));
+                    final String nonNullExpected = expected != null ? expected : source;
+                    final String flags = matcher.group("flags");
+                    data.add(new TestData(line, source, nonNullExpected, flags));
                 }
             }
         }
@@ -72,15 +75,26 @@ public class GenericDataTest {
             GenericDataTest.class.getResourceAsStream("/test.txt"), StandardCharsets.UTF_8));
     }
 
+    public static List<CsvRow> readAll(final String data, final boolean skipEmptyLines) {
+        return CsvReader.builder()
+            .skipEmptyRows(skipEmptyLines)
+            .build(new StringReader(data))
+            .stream()
+            .collect(Collectors.toList());
+    }
+
     static class TestData {
         private final String line;
         private final String source;
         private final String expected;
+        private final boolean skipEmptyLines;
 
-        TestData(final String line, final String source, final String expected) {
+        TestData(final String line, final String source, final String expected,
+                 final String flags) {
             this.line = line;
             this.source = source;
             this.expected = expected;
+            skipEmptyLines = "skipEmptyLines".equals(flags);
         }
 
         public String getLine() {
@@ -93,6 +107,10 @@ public class GenericDataTest {
 
         public String getExpected() {
             return expected;
+        }
+
+        public boolean isSkipEmptyLines() {
+            return skipEmptyLines;
         }
     }
 
