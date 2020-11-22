1d3478c30e385abf7d748ebcd35c847cf7603728
Oliver Siegmar <oliver@siegmar.org>
2020-11-22 12:11:56 +0100

    more realistic work

--- src/jmh/java/de/siegmar/fastcsv/NullWriter.java
+++ src/jmh/java/de/siegmar/fastcsv/NullWriter.java
@@ -1,31 +1,44 @@
 package de.siegmar.fastcsv;
 
 import java.io.Writer;
+import java.util.Arrays;
 
 import org.openjdk.jmh.infra.Blackhole;
 
 /**
  * Writer implementation that sends all data to a black hole.
  */
-class NullWriter extends Writer {
+public class NullWriter extends Writer {
 
-    private final Blackhole blackhole;
+    private static final int BUFFER_SIZE = 8192;
 
-    NullWriter(final Blackhole blackhole) {
-        this.blackhole = blackhole;
+    private final Blackhole bh;
+    private char[] buf = new char[BUFFER_SIZE];
+    private int pos;
+
+    NullWriter(final Blackhole bh) {
+        this.bh = bh;
     }
 
     @Override
     public void write(final char[] cbuf, final int off, final int len) {
-        blackhole.consume(cbuf);
+        if (len + pos > buf.length) {
+            flush();
+        }
+        System.arraycopy(cbuf, off, buf, pos, len);
+        pos += len;
     }
 
     @Override
     public void flush() {
+        bh.consume(Arrays.copyOf(buf, pos));
+        pos = 0;
     }
 
     @Override
     public void close() {
+        flush();
+        buf = null;
     }
 
 }
