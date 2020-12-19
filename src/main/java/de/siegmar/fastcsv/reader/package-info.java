6fa986939da2a51078c7a5ef6bff0aaa13158b58
Oliver Siegmar <oliver@siegmar.de>
2020-12-19 15:54:49 +0100

    add package-info

---
+++ src/main/java/de/siegmar/fastcsv/reader/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * FastCSV reader.
+ *
+ * Obtain low-level (index based) reader via {@link de.siegmar.fastcsv.reader.CsvReader#builder()}
+ * and higher level (name/header based) reader via
+ * {@link de.siegmar.fastcsv.reader.NamedCsvReader#builder()}.
+ */
+package de.siegmar.fastcsv.reader;
