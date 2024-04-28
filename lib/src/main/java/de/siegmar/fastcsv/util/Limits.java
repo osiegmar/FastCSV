8419a802e739d215312ab660d83ab12b7e10c754
Obolrom <65775868+Obolrom@users.noreply.github.com>
2024-04-28 07:42:59 +0300

    Add limits overriding with system properties (#112)

--- lib/src/main/java/de/siegmar/fastcsv/util/Limits.java
+++ lib/src/main/java/de/siegmar/fastcsv/util/Limits.java
@@ -2,6 +2,16 @@ package de.siegmar.fastcsv.util;
 
 /**
  * The {@code Limits} class defines the maximum limits for various fields and records in a CSV file.
+ * <p>
+ * Example use:
+ * <pre>{@code
+ * System.setProperty("fastcsv.max.field.size", "1024");
+ * }</pre>
+ * <p>
+ * Or using VM options:
+ * <pre>{@code
+ * -Dfastcsv.max.field.count=1024
+ * }</pre>
  */
 public final class Limits {
 
@@ -10,13 +20,13 @@ public final class Limits {
      * The value is set to 16,777,216 characters (16 to 64 MiB depending on the circumstance of multibyte character
      * utilization).
      */
-    public static final int MAX_FIELD_SIZE = 16 * 1024 * 1024;
+    public static final int MAX_FIELD_SIZE = getIntProperty("fastcsv.max.field.size", 16 * 1024 * 1024);
 
     /**
      * The {@code MAX_FIELDS_SIZE} constant defines the maximum number of fields per record.
      * The value is set to 16,384.
      */
-    public static final int MAX_FIELD_COUNT = 16 * 1024;
+    public static final int MAX_FIELD_COUNT = getIntProperty("fastcsv.max.field.count", 16 * 1024);
 
     /**
      * The {@code MAX_RECORD_SIZE} constant defines the maximum size for all fields combined in a CSV record.
@@ -27,4 +37,26 @@ public final class Limits {
     private Limits() {
     }
 
+    /**
+     * Retrieves the system property value if presented, otherwise default value is returned.
+     * If the property cannot be parsed as an integer, an {@code IllegalArgumentException} is thrown.
+     *
+     * @param key The system property key.
+     * @param defaultValue The default value to use if the system property is not set or is invalid.
+     * @return The system property value as an integer or the default value if the property is not set or is invalid.
+     * @throws IllegalArgumentException If the system property value cannot be parsed as an integer.
+     */
+    static int getIntProperty(final String key, final int defaultValue) {
+        final String value = System.getProperty(key);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Integer.parseInt(value);
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid format for system property " + key, e);
+        }
+    }
 }
