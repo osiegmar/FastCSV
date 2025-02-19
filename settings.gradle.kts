c36b03f72fe99471e4d36ecd4b85ec9cd7c58e31
Oliver Siegmar <oliver@siegmar.de>
2025-02-19 06:59:12 +0100

    import plugins first

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
-rootProject.name = "FastCSV"
-include("lib", "example")
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
+
+rootProject.name = "FastCSV"
+include("lib", "example")
