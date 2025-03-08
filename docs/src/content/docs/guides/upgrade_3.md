204bcec8a77e6697058aeeafc7e92c2559719ac2
Oliver Siegmar <oliver@siegmar.de>
2025-03-08 18:31:38 +0100

    add FieldModifiers.transform and remove SimpleFieldModifier and upper/lower methods

--- docs/src/content/docs/guides/upgrade_3.md
+++ docs/src/content/docs/guides/upgrade_3.md
@@ -9,5 +9,35 @@ For a full list of changes, including new features, see the [changelog](https://
 
 ## Requirement changes
 
-- The minimum Java version is now 17 (compared to 11 in earlier)
+- The minimum Java version is now 17 (compared to 11 in FastCSV 3.x)
 - This also raised the required Android API level from version 33 (Android 13) to 34 (Android 14)
+
+## SimpleFieldModifier
+
+The `SimpleFieldModifier` class has been removed. It was a simple implementation of the `FieldModifier` interface that is now replaced by the `FieldModifiers#transform(Function<String, String>)` method.
+
+Old way:
+```java
+SimpleFieldModifier normalizeWhitespaces = field -> field.replaceAll("\\s", " ");
+```
+
+New way:
+```java
+FieldModifier normalizeWhitespaces = FieldModifiers.transform(field -> field.replaceAll("\\s", " "));
+```
+
+## Upper and lower case field modifiers
+
+The edge-case methods `FieldModifiers#lower(Locale)` and `FieldModifiers#upper(Locale)` have been removed in favor of a more versatile `FieldModifiers#transform(Function<String, String>)` method.
+
+Old way:
+```java
+FieldModifier toLowerFieldModifier = FieldModifiers.lower(Locale.ENGLISH);
+FieldModifier toUpperFieldModifier = FieldModifiers.upper(Locale.ENGLISH);
+```
+
+New way:
+```java
+FieldModifier toLowerFieldModifier = FieldModifiers.transform(field -> field.toLowerCase(Locale.ENGLISH));
+FieldModifier toUpperFieldModifier = FieldModifiers.transform(field -> field.toUpperCase(Locale.ENGLISH));
+```
--- lib/src/intTest/java/blackbox/reader/FieldModifierTest.java
+++ lib/src/intTest/java/blackbox/reader/FieldModifierTest.java
@@ -3,8 +3,6 @@ package blackbox.reader;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Locale;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -15,7 +13,6 @@ import de.siegmar.fastcsv.reader.CsvReader;
 import de.siegmar.fastcsv.reader.CsvRecordHandler;
 import de.siegmar.fastcsv.reader.FieldModifier;
 import de.siegmar.fastcsv.reader.FieldModifiers;
-import de.siegmar.fastcsv.reader.SimpleFieldModifier;
 import testutil.CsvRecordAssert;
 
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
@@ -56,52 +53,18 @@ class FieldModifierTest {
             .containsExactly("foo", "bar", "baz");
     }
 
-    @SuppressWarnings("removal")
-    @Test
-    void lower() {
-        final CsvRecordHandler cbh = CsvRecordHandler.of(c -> c.fieldModifier(FieldModifiers.lower(Locale.ROOT)));
-
-        assertThat(crb.build(cbh, "FOO,BAR").stream())
-            .singleElement(CsvRecordAssert.CSV_RECORD)
-            .fields()
-            .containsExactly("foo", "bar");
-    }
-
-    @SuppressWarnings("removal")
-    @Test
-    void upper() {
-        final CsvRecordHandler cbh = CsvRecordHandler.of(c -> c.fieldModifier(FieldModifiers.upper(Locale.ROOT)));
-
-        assertThat(crb.build(cbh, "foo,bar").stream())
-            .singleElement(CsvRecordAssert.CSV_RECORD)
-            .fields()
-            .containsExactly("FOO", "BAR");
-    }
-
-    @SuppressWarnings("removal")
-    @Test
-    void simple() {
-        final CsvRecordHandler cbh = CsvRecordHandler.of(c -> c
-            .fieldModifier((SimpleFieldModifier) field -> "<" + field + ">"));
-
-        assertThat(crb.build(cbh, "foo,bar").stream())
-            .singleElement(CsvRecordAssert.CSV_RECORD)
-            .fields()
-            .containsExactly("<foo>", "<bar>");
-    }
-
     @Test
     void combination() {
         crb.commentStrategy(CommentStrategy.READ);
 
-        final FieldModifier modifier = FieldModifiers.modify(field -> field.toLowerCase(Locale.ROOT))
-            .andThen(FieldModifiers.TRIM);
+        final FieldModifier modifier = FieldModifiers.TRIM
+            .andThen(FieldModifiers.transform(field -> "<" + field + ">"));
 
         final CsvRecordHandler cbh = CsvRecordHandler.of(c -> c.fieldModifier(modifier));
 
-        assertThat(crb.build(cbh, "FOO, bar , BAZ  \n# foo ").stream())
+        assertThat(crb.build(cbh, "foo, bar , baz  \n# foo ").stream())
             .satisfiesExactly(
-                item -> CsvRecordAssert.assertThat(item).fields().containsExactly("foo", "bar", "baz"),
+                item -> CsvRecordAssert.assertThat(item).fields().containsExactly("<foo>", "<bar>", "<baz>"),
                 item -> CsvRecordAssert.assertThat(item).isComment().fields().containsExactly(" foo ")
             );
     }
