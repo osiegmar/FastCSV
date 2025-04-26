8a13ac036565713f0a79d6568f807689c0834f6c
Oliver Siegmar <oliver@siegmar.de>
2025-04-26 03:49:57 +0200

    use Java 24

--- buildSrc/src/main/kotlin/fastcsv.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/fastcsv.java-conventions.gradle.kts
@@ -9,6 +9,11 @@ repositories {
     mavenCentral()
 }
 
+spotbugs {
+    // Version bundled with Gradle is not able to run on Java 24.
+    toolVersion = "4.9.3"
+}
+
 tasks.withType<com.github.spotbugs.snom.SpotBugsTask>().configureEach {
     excludeFilter = file("${project.rootDir}/config/spotbugs/config.xml")
     reports.maybeCreate("xml").required = false
@@ -16,8 +21,8 @@ tasks.withType<com.github.spotbugs.snom.SpotBugsTask>().configureEach {
 }
 
 pmd {
-    // Version bundled with Gradle is not able to run on Java 23.
-    toolVersion = "7.10.0"
+    // Version bundled with Gradle is not able to run on Java 24.
+    toolVersion = "7.12.0"
     isConsoleOutput = true
     ruleSets = emptyList()
     ruleSetFiles = files("${project.rootDir}/config/pmd/config.xml")
