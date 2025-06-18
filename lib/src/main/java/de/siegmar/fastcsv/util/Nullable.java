52fe46d02aef2dec14bf43b86bf43015c4b8ec7d
Oliver Siegmar <oliver@siegmar.de>
2025-06-18 11:04:21 +0200

    refactor: add class retention to allow incremental builds

--- lib/src/main/java/de/siegmar/fastcsv/util/Nullable.java
+++ lib/src/main/java/de/siegmar/fastcsv/util/Nullable.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /// Annotation to indicate that a method parameter, return value, or field can be null.
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 public @interface Nullable {
 }
